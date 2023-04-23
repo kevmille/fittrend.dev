76cccc259456315b3c2cd89536c13a3dd3979061
Kevin Matsunaga <kevinmillerjr@gmail.com>
2023-04-23 10:43:28 -0700

    Added TailwindUI plugins and React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
 import svelte from "@astrojs/svelte";
-
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   // ...
   site: "https://fittrend.dev",
-  integrations: [tailwind(), svelte(), sitemap(), partytown()],
+  integrations: [tailwind(), svelte(), sitemap(), partytown(), react()],
   output: "server",
   adapter: vercel()
 });
