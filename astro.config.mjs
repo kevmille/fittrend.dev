73fb3e6f94c8d2a77fa98b3faff2223d9ca67aee
Kevin Matsunaga <kevinmillerjr@gmail.com>
2023-04-25 15:51:16 -0700

    Fetch data for OFR with Svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,9 @@ import svelte from "@astrojs/svelte";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
-
 import robotsTxt from "astro-robots-txt";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
---
+++ src/components/OFR.svelte
@@ -0,0 +1,5 @@
+<script>
+	fetch('https://data.financialresearch.gov/v1/series/dataset?dataset=tyld&start_date=2020-07-23')
+		.then(response => response.json())
+		.then(data => console.log(data))
+</script>
