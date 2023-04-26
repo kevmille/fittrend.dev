fbee9432db625e1a4d5b6bb19e349003b9fd8c61
Kevin Matsunaga <kevinmillerjr@gmail.com>
2023-04-25 19:48:31 -0700

    Got Astro fetch api to work

--- src/components/OFR.svelte
+++ src/components/OFR.svelte
@@ -1,5 +1,23 @@
 <script>
-	fetch('https://data.financialresearch.gov/v1/series/dataset?dataset=tyld&start_date=2020-07-23')
-		.then(response => response.json())
-		.then(data => console.log(data))
+import axios from 'axios';
+
+let fetchedData = null;
+
+async function fetchData() {
+  try {
+    const response = await axios.get('https://data.financialresearch.gov/v1/series/dataset?dataset=tyld&start_date=2020-07-23');
+    const fetchedData = response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchData();
 </script>
+
+<!-- Render the fetched data in your template -->
+{#if fetchedData}
+  <h3>{fetchedData.timeseries}</h3>
+{:else}
+  <p>Loading data...</p>
+{/if}
